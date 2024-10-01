1252c49d113a2b38feaa5626698871d24acdb4c1
Chaima <amirou.septiembre@gmail.com>
2024-10-01 19:00:01 +0200

    eeeee

--- app/src/main/java/com/example/myfirstapplication/Ejer2Kotlin.kt
+++ app/src/main/java/com/example/myfirstapplication/Ejer2Kotlin.kt
@@ -8,15 +8,58 @@ package com.example.myfirstapplication
 fun main()
 {
     println("Introduce un numero entero positivo")
-    val numero = readln().toInt() //si no haces la conversion te salta excepcion
-//try catch para evitar la excepcion de los decimales!!!!
-    while (numero < 0)
+    val numero : Int
+    try {
+        numero = readln().toInt() //si no haces la conversion te salta excepcion
+        //try catch para evitar la excepcion de los decimales!!!!
+        if (numero<0)
+        {
+            println("Debe de ser un numero positivo")
+        }else
+        {
+            esPrimo(numero)
+            esPar(numero)
+            esCuadratico(numero)
+        }
+
+
+    }catch (e: NumberFormatException)
     {
-        println("Debe de ser un numero positivo")
+        println("Error: Debe de ser un numero positivo")
     }
+
 }
 
-fun Apart1()
+fun esPrimo(x:Int)
 {
+    //dividir entre los numeros 1 y x/2
+    var div : Int = 1
+    while (div<=x/2)
+    {
+        if (x % div == 0)
+        {
+            return println("$x NO es primo");
+        }
+        div++;
+    }
+    return  println("$x es primo")
+}
 
+fun esPar(x: Int)
+{
+    if (x %2 == 0)
+        return println("$x es par")
+    return println("$x es impar")
+}
+
+fun esCuadratico(num: Int)
+{
+    for (x in 1..num/2)
+    {
+        if (x*x == num)
+        {
+            return println("$num es cuadratico")
+        }
+    }
+    println("$num No es cuadratico")
 }
